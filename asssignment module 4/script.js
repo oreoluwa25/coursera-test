6402084303b365d04f7333f52d00a4179182e5f7
oreoluwa25 <69446441+oreoluwa25@users.noreply.github.com>
2020-08-19 11:56:23 +0100

    Update script.js

--- asssignment module 4/script.js
+++ asssignment module 4/script.js
@@ -36,7 +36,7 @@ for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
 
   if (firstLetter === 'j') {
-    byeSpeaker.(names[i]);
+    byeSpeaker.speak(names[i]);
   }
   else {
     helloSpeaker.speak(names[i]);
